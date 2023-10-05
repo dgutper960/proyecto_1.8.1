1a11611a961d5c801118d5eef1ce84cb86d524b8
dgutper960 <dgutper960@outlook.es>
2023-10-05 11:10:30 +0200

    inicio tarea

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'proyecto_1.8.1',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
