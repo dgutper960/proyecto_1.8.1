791c949f6eb1ef0c52e1dd55d8ad4667c6ca9883
dgutper960 <dgutper960@outlook.es>
2023-10-05 11:30:49 +0200

    clase 05/10/23

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,11 +2,18 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.page.html',
+  templateUrl: 'home.page.html', //Asociación de los archivos html y js
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
 
+  //almacenamos la entrada del susuario
+  num: number = 0;
+
+
+
   constructor() {}
+
+
 
 }
