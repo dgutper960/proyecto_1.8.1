9d88418b98f3204450f2fb730dbf06f2e9ec2ce8
dgutper960 <dgutper960@outlook.es>
2023-10-18 08:45:40 +0200

    a

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,18 +2,22 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.page.html', //Asociación de los archivos html y js
-  styleUrls: ['home.page.scss'],
+  templateUrl: 'home.page.html', //Asociación a archivo html
+  styleUrls: ['home.page.scss'], //Asociación a archivo css
 })
 export class HomePage {
 
   //almacenamos la entrada del susuario
-  num: number = 0;
+  num: number = 0; // de tipo number
+  numBinario: number = 0;
 
 
 
   constructor() {}
 
-
+  // creamos el método para pasar a binario
+  conversorBinario(num){
+    
+  }
 
 }
